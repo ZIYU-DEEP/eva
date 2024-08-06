a5c98b9ee7c565875d009df61c3c15f66253b3d1
Ziyu <ziyuye@uchicago.edu>
2024-08-06 02:21:12 -0400

    update run 2

--- benchmark/arena-hard/transient_scripts/run_2.sh
+++ benchmark/arena-hard/transient_scripts/run_2.sh
@@ -4,17 +4,15 @@ set -x
 
 MODEL_PATH="cat-searcher/gemma-2-9b-it-sppo-iter-2"
 MODEL_NAME="gemma-2-9b-it-sppo-iter-2"
-cuda_visible_devices="2"
 port=8002
 dtype="bfloat16"
-tensor_parallel_size=1
 
-CUDA_VISIBLE_DEVICES=2 python gen_answer.py \
+python gen_answer.py \
     --setting-file config/gen_answer_config_2.yaml \
     --endpoint-file config/api_config.yaml
 
-CUDA_VISIBLE_DEVICES=2 python gen_judgment.py \
+python gen_judgment.py \
     --setting-file config/judge_config_2.yaml \
     --endpoint-file config/api_config.yaml
 
-CUDA_VISIBLE_DEVICES=2 python show_result.py
+python show_result.py
