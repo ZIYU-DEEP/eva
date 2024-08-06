3203bd7dc107e658ae9ca748e07c1ce300eb616e
Ziyu <ziyuye@uchicago.edu>
2024-08-06 17:55:00 -0400

    alpaca-eval-2

--- benchmark/alpaca_eval/run.sh
+++ benchmark/alpaca_eval/run.sh
@@ -6,5 +6,6 @@ echo ${CONFIG_PATH}
 
 alpaca_eval evaluate_from_model \
   --model_configs $CONFIG_PATH \
-  --annotators_config 'alpaca_eval_gpt4_turbo_fn'   
+  --annotators_config 'weighted_alpaca_eval_gpt4_turbo' \
+  --reference_output 'gpt4_turbo'
  
