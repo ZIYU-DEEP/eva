907431c99ac8f56b5d163d3eed703628b6d15594
Ziyu <ziyuye@uchicago.edu>
2024-08-06 18:06:21 -0400

    fix run

--- benchmark/alpaca_eval/run.sh
+++ benchmark/alpaca_eval/run.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
 # Make sure you run at the alpaca_eval benchmark directory.
 # e.g., YOUR_ROOT/benchmark/alpaca_eval
 
+# Define CONFIG_PATH to point to your configuration directory
 CONFIG_PATH=$(pwd)/models_configs/gemma-2-9b-it-sppo-iter-0
+export CONFIG_PATH
 echo ${CONFIG_PATH}
 
+# Substitute the environment variables in configs.yaml and save to local_configs.yaml
+envsubst '${CONFIG_PATH}' < "${CONFIG_PATH}/configs.yaml" > "${CONFIG_PATH}/local_configs.yaml"
+
+# Run alpaca_eval with the substituted configuration file
 alpaca_eval evaluate_from_model \
-  --model_configs $CONFIG_PATH \
+  --model_configs "${CONFIG_PATH}/local_configs.yaml" \
   --annotators_config 'weighted_alpaca_eval_gpt4_turbo' \
   --reference_output 'gpt4_turbo'
- 
