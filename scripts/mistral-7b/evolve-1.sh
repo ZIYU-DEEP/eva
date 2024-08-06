27aa85b0c2bd06e5487db8397fa63a623002277f
Ziyu <ziyuye@uchicago.edu>
2024-08-06 13:09:54 -0400

    re-run

--- scripts/mistral-7b/evolve-1.sh
+++ scripts/mistral-7b/evolve-1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-# set -x  # Print the commands
+set -x  # Print the commands
 
-bash ./scripts/mistral-7b/evolve-create-gen-iter-1.sh
-bash ./scripts/mistral-7b/evolve-create-iter-1.sh
+# bash ./scripts/mistral-7b/evolve-create-gen-iter-1.sh
+# bash ./scripts/mistral-7b/evolve-create-iter-1.sh
 bash ./scripts/mistral-7b/evolve-gen-iter-1.sh
 bash ./scripts/mistral-7b/evolve-train-iter-1.sh
