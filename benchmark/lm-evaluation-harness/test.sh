3fded712d7a0ac7700c7dbe39d9131d1da087346
Ziyu <ziyuye@uchicago.edu>
2024-08-10 19:54:49 -0400

    test

--- benchmark/lm-evaluation-harness/test.sh
+++ benchmark/lm-evaluation-harness/test.sh
@@ -2,7 +2,7 @@ lm_eval --model hf \
     --model_args pretrained=pretrained=google/gemma-2-9b-it \
     --tasks hellaswag \
     --device cuda:0,1,2,3,4,5,6,7 \
-    --batch_size auto:4 \
+    --batch_size 4 \
     --log_samples \
     --output_path results \
-    --hf_hub_log_args hub_results_org=cat-searcher,hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
+    # --hf_hub_log_args hub_results_org=cat-searcher,hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
