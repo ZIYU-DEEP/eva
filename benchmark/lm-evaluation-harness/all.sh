aa49085c786b5e8ead816ea0a392613f6ccb0f86
Ziyu <ziyuye@uchicago.edu>
2024-08-10 23:03:47 -0400

    trust

--- benchmark/lm-evaluation-harness/all.sh
+++ benchmark/lm-evaluation-harness/all.sh
@@ -13,6 +13,7 @@ accelerate launch -m lm_eval --model hf \
     --device cuda:0,1,2,3,4,5,6,7 \
     --batch_size 4 \
     --log_samples \
+    --trust_remote_code \
     --output_path results \
     --hf_hub_log_args hub_results_org=cat-searcher,hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
 
@@ -24,6 +25,7 @@ accelerate launch -m lm_eval --model hf \
     --device cuda:0,1,2,3,4,5,6,7 \
     --batch_size 4 \
     --log_samples \
+    --trust_remote_code \
     --output_path results \
     --hf_hub_log_args hub_results_org=cat-searcher,hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
 
@@ -36,6 +38,7 @@ accelerate launch -m lm_eval --model hf \
     --device cuda:0,1,2,3,4,5,6,7 \
     --batch_size 4 \
     --log_samples \
+    --trust_remote_code \
     --output_path results \
     --hf_hub_log_args hub_results_org=cat-searcher,hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
 
