d747c5e95294988076938bf640df9d62b603b2b3
Ziyu <ziyuye@uchicago.edu>
2024-08-10 20:19:40 -0400

    typo

--- benchmark/lm-evaluation-harness/acc-1-evol.sh
+++ benchmark/lm-evaluation-harness/acc-1-evol.sh
@@ -1,6 +1,6 @@
 accelerate launch -m lm_eval --model hf \
     --model_args pretrained=cat-searcher/gemma-2-9b-it-sppo-iter-1-evol-1 \
-    --tasks arc \
+    --tasks arc_challenge \
     --device cuda:0,1,2,3,4,5,6,7 \
     --batch_size 4 \
     --log_samples \
