ed3a1c6cdbd38112ecdf7d7645e9957344dd760b
Ziyu <ziyuye@uchicago.edu>
2024-08-16 19:07:42 -0400

    arena hard

--- benchmark/arena-hard/sglang_scripts/serve_dpo_iter_1_evol_1.sh
+++ benchmark/arena-hard/sglang_scripts/serve_dpo_iter_1_evol_1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-model_name=${1:-"cat-searcher/gemma-2-9b-it-dpo-iter-0"}
+model_name=${1:-"cat-searcher/gemma-2-9b-it-dpo-iter-1-evol-1"}
 dtype=${2:-"bfloat16"}
 host=${3:-"localhost"}
-port=${4:-8000}
+port=${4:-8964}
 tensor_parallel_size=${5:-8}
 attention_backend=${6:-"FLASHINFER"}  # Use FLASHINFER for gemma-2 models and XFORMERS for other models
 cuda_visible_devices=${7:-"0,1,2,3,4,5,6,7"}
