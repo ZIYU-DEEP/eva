b7edbd28cffeb9f65982b0c02e4beaabc8a98203
Ziyu <ziyuye@uchicago.edu>
2024-08-19 04:56:29 -0400

    api call hang

--- scripts/gemma-2-9b-it-dpo-split-by-3/evolve-create-iter-1-debug.sh
+++ scripts/gemma-2-9b-it-dpo-split-by-3/evolve-create-iter-1-debug.sh
@@ -40,7 +40,7 @@ NUM_EVOLUTIONS=${NUM_EVOLUTIONS:-4}
 MAX_PROMPT_LENGTH=${MAX_PROMPT_LENGTH:-666}  # Test longer length
 EVOLVE_TEMPERATURE=${EVOLVE_TEMPERATURE:-0.88}  # Test lower temperature
 SAMPLE_METHOD=${SAMPLE_METHOD:-'importance_weighted'}
-GEN_MODEL_NAME=${GEN_MODEL_NAME:-'gpt-4-0125-preview'}
+GEN_MODEL_NAME=${GEN_MODEL_NAME:-'gpt-4o-mini'}
 # ------------------------------------------------------------------
 
 # ------------------------------------------------------------------
@@ -117,7 +117,7 @@ python src/evolve_prompt.py \
     --data_root $DATA_ROOT \
     --gen_model_name $GEN_MODEL_NAME \
     --num_evolutions $NUM_EVOLUTIONS \
-    --num_workers 1 \
+    --num_workers 10 \
     --do_adaptive_sample 1 \
     --sample_metric $SAMPLE_METRIC \
     --sample_frac $SAMPLE_FRAC \
