c31442422c6753330f9a6843a1e0f5cfd85dfec6
Ziyu <ziyuye@uchicago.edu>
2024-08-23 06:30:18 -0400

    smaller beta

--- scripts/gemma-2-9b-it-rpo-split-by-3/evolve-train-iter-1.sh
+++ scripts/gemma-2-9b-it-rpo-split-by-3/evolve-train-iter-1.sh
@@ -18,7 +18,7 @@ PREF=${PREF:-"rpo_score"}
 # Other general parameter to be reset
 HF_USERNAME=${HF_USERNAME:-'cat-searcher'}
 LEARNING_RATE=${LEARNING_RATE:-"5.0e-7"}
-BETA=${BETA:-"0.1"}
+BETA=${BETA:-"0.05"}
 OPTIM=${OPTIM:-"adamw_torch"}
 N_EPOCHS=${N_EPOCHS:-1}
 BATCH_SIZE=${BATCH_SIZE:-1}
