1faad7d463020a5d3568740e45067efb367e7d64
Ziyu <ziyuye@uchicago.edu>
2024-08-27 14:49:10 -0400

    make a shorter name

--- scripts/gemma-2-9b-it-dpo-variant/evolve-train-iter-1-gap-iw-topic.sh
+++ scripts/gemma-2-9b-it-dpo-variant/evolve-train-iter-1-gap-iw-topic.sh
@@ -61,7 +61,8 @@ DATASET="${DATASET_BASE}-evol-${SAMPLE_METRIC}-${SAMPLE_FRAC}-mixed-${RATIO_BASE
 
 if [ "$SAMPLE_METHOD" = 'iw_topic' ]; then
     DATASET_EVOL="${DATASET_PREFIX}-evol-${SAMPLE_METRIC}-${SAMPLE_FRAC}-iw_topic-${IW_TOPIC_COEF}-pair"
-    DATASET="${DATASET_BASE}-evol-${SAMPLE_METRIC}-${SAMPLE_FRAC}-iw_topic-${IW_TOPIC_COEF}-mixed-${RATIO_BASE}-${RATIO_EVOL}-pair"
+    # DEBUG SHORTEN THE NAME
+    DATASET="uf-1-6-dpo-evol-${SAMPLE_METRIC}-${SAMPLE_FRAC}-iw_topic-${IW_TOPIC_COEF}-mixed-${RATIO_BASE}-${RATIO_EVOL}-pair"
 fi
 
 # The directory for the saved model
