8a37cafa05f422accbb7938ef1457865d61a2f88
Ziyu <ziyuye@uchicago.edu>
2024-08-27 05:37:03 -0400

    fix

--- benchmark/mt_bench/run_rpo.sh
+++ benchmark/mt_bench/run_rpo.sh
@@ -15,16 +15,16 @@ _MODEL_ID="NSPLIT3-gemma-2-9b-it-rpo-iter-1-evol-1"
 # MODEL_PATHS+=($_MODEL)
 # MODEL_IDS+=($_MODEL_ID)
 
-python download_model.py \
-    --model-path $_MODEL \
-    --num-gpus-total 8 \
-    --dtype bfloat16
+# python download_model.py \
+#     --model-path $_MODEL \
+#     --num-gpus-total 8 \
+#     --dtype bfloat16
 
-python gen_model_answer.py \
-    --model-path $_MODEL \
-    --model-id $_MODEL_ID \
-    --num-gpus-total 8 \
-    --dtype bfloat16
+# python gen_model_answer.py \
+#     --model-path $_MODEL \
+#     --model-id $_MODEL_ID \
+#     --num-gpus-total 8 \
+#     --dtype bfloat16
 
 # 3. Generate judgement
 python gen_judgment_single.py \
