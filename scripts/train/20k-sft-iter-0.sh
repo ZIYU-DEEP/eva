4b5565af11078e97ee5bb1de5af6b4e51bd943be
Ziyu <ziyuye@uchicago.edu>
2024-08-30 06:33:07 -0400

    sft

---
+++ scripts/train/20k-sft-iter-0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+# set -x  # Print the commands
+
+# Set the environmental variable
+export WANDB_PROJECT="sft"
+
+# ##################################################################
+# 1. Training
+# ##################################################################
+# ------------------------------------------------------------------
+# Run the training
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+    --config_file ./recipes/accelerate_configs/deepspeed_zero3.yaml \
+    --main_process_port 8964 \
+    eva/run_sft.py "./recipes/sft/config_full.yaml" \
+    --learning_rate=2.0e-05 \
+    --optim="adamw_torch" \
+    --output_dir="checkpoints/NSPLIT3-gemma-2-9b-it-sft-iter-0" \
+    --model_name_or_path="cat-searcher/NSPLIT3-gemma-2-9b-it-sft-iter-0" \
+    --per_device_train_batch_size=4 \
+    --gradient_accumulation_steps=4 \
+    --num_train_epochs=3
+# ------------------------------------------------------------------
