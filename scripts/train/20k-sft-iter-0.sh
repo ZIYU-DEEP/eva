a9735307e3867325e49e4d7116d2d2912c903e6f
Ziyu <ziyuye@uchicago.edu>
2024-08-30 07:14:08 -0400

    add model path

--- scripts/train/20k-sft-iter-0.sh
+++ scripts/train/20k-sft-iter-0.sh
@@ -5,6 +5,9 @@ set -e
 # Set the environmental variable
 export WANDB_PROJECT="sft"
 
+SFT_MODEL_PATH="google/gemma-2-9b-it"
+MODEL_PATH=${SFT_MODEL_PATH}
+
 # ##################################################################
 # 1. Training
 # ##################################################################
@@ -17,7 +20,7 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
     --learning_rate=2.0e-05 \
     --optim="adamw_torch" \
     --output_dir="checkpoints/NSPLIT3-gemma-2-9b-it-sft-iter-0" \
-    --model_name_or_path="cat-searcher/NSPLIT3-gemma-2-9b-it-sft-iter-0" \
+    --model_name_or_path=${MODEL_PATH} \
     --per_device_train_batch_size=4 \
     --gradient_accumulation_steps=4 \
     --num_train_epochs=3
