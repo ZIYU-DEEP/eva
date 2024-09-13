e7237dcb39c0ddc3b56d7b1a6e00c6daadcad453
Ziyu <ziyuye@uchicago.edu>
2024-09-13 04:42:33 -0400

    ah

--- eva/run_dpo_online.py
+++ eva/run_dpo_online.py
@@ -124,7 +124,8 @@ if __name__ == "__main__":
             return False  # Invalid prompt
         return True  # Valid prompt
 
-    dataset = dataset.filter(check_prompt_message, num_proc=6)
+    dataset = dataset.filter(check_prompt_message, 
+                             num_proc=training_args.dataset_num_proc)
     print('Dataset is now filtered!')
 
 
--- scripts/online-dpo.sh
+++ scripts/online-dpo.sh
@@ -2,14 +2,14 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
     --config_file ./recipes/accelerate_configs/deepspeed_zero3.yaml \
     --main_process_port 8964 \
     eva/run_dpo_online.py \
-    --model_name_or_path google/gemma-2-9b-it  \
-    --reward_model_path trl-lib/pythia-1b-deduped-tldr-rm \
-    --dataset_name cat-searcher/ultrafeedback-dpo-gemma-2-9b-it-split-1-iter-1-pair-evol-reward_var-0.25-mixed-0.2-0.8-pair \
+    --model_name_or_path trl-lib/pythia-2.8b-deduped-tldr-sft    \
+    --reward_model_path trl-lib/pythia-2.8b-deduped-tldr-rm \
+    --dataset_name trl-lib/tldr \
     --learning_rate 5.0e-7 \
     --lr_scheduler_type "cosine" \
     --output_dir checkpoints/pythia-1b-deduped-tldr-online-dpo \
     --beta 0.1 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 4 \
     --num_train_epochs 1 \
     --optim "adamw_torch" \
