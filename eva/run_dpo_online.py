4321d7a92b86b3ea2544ced677ff8f3e5a38f12a
Ziyu <ziyuye@uchicago.edu>
2024-09-13 04:57:12 -0400

    from raptors

--- eva/run_dpo_online.py
+++ eva/run_dpo_online.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
 
     # -------------------------------------------------------------------------
     # SET THE PROMPTS
-    dataset = load_dataset(args.dataset_name)[args.dataset_train_split][:1000]  #TODO
+    dataset = load_dataset(args.dataset_name)[args.dataset_train_split].select(range(0, 160))  #TODO
 
 
     def check_prompt_message(example):
