853ecc25f5940fcc55185b8fabe5b5ec8f0485c8
Ziyu <ziyuye@uchicago.edu>
2024-09-13 01:46:58 -0400

    from raptors

--- eva/run_dpo_online.py
+++ eva/run_dpo_online.py
@@ -15,18 +15,25 @@
 """
 Usage:
 
-python run_dpo_online.py \
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+    --config_file ./recipes/accelerate_configs/deepspeed_zero3.yaml \
+    --main_process_port 8964 \
+    eva/run_dpo_online.py \
     --model_name_or_path trl-lib/pythia-1b-deduped-tldr-sft  \
     --reward_model_path trl-lib/pythia-1b-deduped-tldr-rm \
     --dataset_name trl-lib/tldr \
     --learning_rate 5.0e-7 \
-    --output_dir checkpoints/pythia-1b-tldr-online-dpo \
-    --per_device_train_batch_size 4 \
-    --gradient_accumulation_steps 32 \
+    --output_dir checkpoints/pythia-1b-deduped-tldr-online-dpo \
+    --beta 0.1 \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 2 \
     --num_train_epochs 3 \
     --max_new_tokens 53 \
     --warmup_ratio 0.1 \
-    --missing_eos_penalty 1.0
+    --missing_eos_penalty 1.0 \
+    --logging_steps 20 \
+    --save_steps 0.1 \
+    --push_to_hub
 """
 
 import torch
