fd37929221635c6286712d93266a5d2a7384f871
Ziyu <ziyuye@uchicago.edu>
2024-09-13 03:47:53 -0400

    proceed to training but oom

--- eva/run_dpo_online.py
+++ eva/run_dpo_online.py
@@ -25,8 +25,8 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
     --learning_rate 5.0e-7 \
     --output_dir checkpoints/pythia-1b-deduped-tldr-online-dpo \
     --beta 0.1 \
-    --per_device_train_batch_size 8 \
-    --gradient_accumulation_steps 2 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 8 \
     --num_train_epochs 3 \
     --max_new_tokens 53 \
     --warmup_ratio 0.1 \
