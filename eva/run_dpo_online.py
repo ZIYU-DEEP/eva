fecfe559b462451bdfc719f4a685aee5f4fe7d44
Ziyu <ziyuye@uchicago.edu>
2024-09-13 04:15:15 -0400

    hang

--- eva/run_dpo_online.py
+++ eva/run_dpo_online.py
@@ -25,17 +25,17 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
     --learning_rate 5.0e-7 \
     --output_dir checkpoints/pythia-1b-deduped-tldr-online-dpo \
     --beta 0.1 \
-    --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 4 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 8 \
     --num_train_epochs 1 \
-    --max_new_tokens 53 \
+    --max_new_tokens 1024 \
     --warmup_ratio 0.1 \
     --missing_eos_penalty 1.0 \
     --logging_steps 20 \
     --save_steps 0.1 \
     --gradient_checkpointing \
-    --bf16 \
-    --push_to_hub
+    --bf16 
+    # --push_to_hub
 """
 
 import torch
