68c6c39b8a812acbc7a830136ad6ac0e434b1ad0
Ziyu <ziyuye@uchicago.edu>
2024-09-16 15:48:06 -0400

    debug

--- debug.py
+++ debug.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 
 def main():
     # Load the model and tokenizer
-    model_name = 'google/gemma-2-2b-it'
+    model_name = 'google/gemma-2-9b-it'
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
     model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
 
