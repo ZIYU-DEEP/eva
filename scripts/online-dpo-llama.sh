bf07246c11f9ef97ee681f7ff6c6798b93ecd0e2
Ziyu <ziyuye@uchicago.edu>
2024-09-16 21:39:12 -0400

    puzzled

--- scripts/online-dpo-llama.sh
+++ scripts/online-dpo-llama.sh
@@ -3,7 +3,7 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
     --config_file ./recipes/accelerate_configs/deepspeed_zero3.yaml \
     --main_process_port 8964 \
     eva/run_dpo_online.py \
-    --model_name_or_path "google/gemma-2-2b-it"   \
+    --model_name_or_path "meta-llama/Meta-Llama-3.1-8B-Instruct"   \
     --reward_model_path "Skywork/Skywork-Reward-Llama-3.1-8B"\
     --dataset_name cat-searcher/ultrafeedback-gemma-split-1 \
     --hub_model_id "cat-searcher/llama-3.1-8b-it-odpo-rm-8b" \
