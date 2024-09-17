eb128ab48995051e14cc26e50d7b7126eaf2ada1
Ziyu <ziyuye@uchicago.edu>
2024-09-16 21:23:16 -0400

    puzzled

--- scripts/online-dpo.sh
+++ scripts/online-dpo.sh
@@ -1,5 +1,4 @@
 
-
 ACCELERATE_LOG_LEVEL=info accelerate launch \
     --config_file ./recipes/accelerate_configs/deepspeed_zero3.yaml \
     --main_process_port 8964 \
@@ -7,15 +6,16 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
     --model_name_or_path "google/gemma-2-2b-it"   \
     --reward_model_path "Skywork/Skywork-Reward-Gemma-2-27B"\
     --dataset_name cat-searcher/ultrafeedback-gemma-split-1 \
+    --hub_model_id "cat-searcher/gemma-2-2b-it-odpo-rm-27b" \
+    --output_dir checkpoints/gemma-2-2b-it-odpo-rm-27b \
     --learning_rate 5.0e-7 \
     --lr_scheduler_type "cosine" \
-    --output_dir checkpoints/gemma-2-2b-it-odpo-rm-27b \
     --beta 0.1 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
     --num_train_epochs 1 \
     --optim "adamw_torch" \
-    --max_new_tokens 1024 \
+    --max_new_tokens 512 \
     --warmup_ratio 0.1 \
     --missing_eos_penalty 1.0 \
     --logging_steps 20 \
@@ -26,7 +26,6 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
     --n_completions 2 \
     --bf16 \
     --push_to_hub \
-    --hub_model_id "cat-searcher/gemma-2-2b-it-odpo-rm-27b" \
     --hub_private_repo \
     --attn_implementation "eager" \
     # --push_to_hub
