ae5bf5715f55f3f4990449def4cbba1544289580
Ziyu <ziyuye@uchicago.edu>
2024-10-28 13:46:52 -0400

    typo

--- benchmark/arena-hard/scripts/run/run-llama.sh
+++ benchmark/arena-hard/scripts/run/run-llama.sh
@@ -37,15 +37,15 @@ awk -v model_name="$MODEL_NAME" '
 
 # -----------------------------------------------------------------------
 # Generate answer for the specific model using the temporary config file
-python gen_answer.py \
-    --setting-file $TEMP_CONFIG_GEN \
-    --endpoint-file config/api_config.yaml
+# python gen_answer.py \
+#     --setting-file $TEMP_CONFIG_GEN \
+#     --endpoint-file config/api_config.yaml
 # -----------------------------------------------------------------------
 
 # Stop the sglang server
-echo "Stopping the server..."
-pkill -9 -f "sglang.launch_server"
-echo "Server stopped."
+# echo "Stopping the server..."
+# pkill -9 -f "sglang.launch_server"
+# echo "Server stopped."
 
 # -----------------------------------------------------------------------
 # Generate judgement for the specific model using the temporary config file
